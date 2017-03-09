11efddbc4fe7404fa5eca4c3393353fa28966160
Alex Pacheco <alex.pacheco.s@gmail.com>
2017-03-09 20:36:35 -0300

    listos los componentes message y messageList con props y css

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import 'normalize-css'
 
+import styles from './app.css'
 import Header from '../Header'
 import Main from '../Main'
+
 class App extends Component {
   render(){
     return(
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import MessageList from '../MessageList'
 
 class Main extends Component {
+  constructor(){
+    super()
+    this.state = {
+      messages: [
+        {
+          text: 'Mensaje de prueba',
+          picture: 'https://pbs.twimg.com/profile_images/702215975068893184/93UyNcuZ.jpg',
+          displayName: 'Alex Pacheco',
+          userName: 'almapase',
+          date: Date.now() - 180000
+        },
+        {
+          text: 'Este es un nuevo mensaje',
+          picture: 'https://pbs.twimg.com/profile_images/702215975068893184/93UyNcuZ.jpg',
+          displayName: 'Alex Pacheco',
+          userName: 'almapase',
+          date: Date.now() - 1800000
+        }
+      ]
+    }
+  }
+
   render(){
     return(
-      <div>Main section</div>
+      <MessageList messages={this.state.messages} />
     )
   }
 }
